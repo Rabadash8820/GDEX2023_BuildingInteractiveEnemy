bbeb1dc0b7d5a30b1d71d17538dfdba1eac9b4a1
Dan Vicarel <shundra8820@gmail.com>
2023-05-12 03:39:20 -0400

    Player prefab now destroys itself when Health is depleted

--- SpaceGame/Assets/SpaceGame/scripts/DestroyerObject.cs
+++ SpaceGame/Assets/SpaceGame/scripts/DestroyerObject.cs
@@ -5,7 +5,7 @@ namespace SpaceGame
     [CreateAssetMenu(menuName = nameof(SpaceGame) + "/" + nameof(DestroyerObject), fileName = "destroyer")]
     public class DestroyerObject : ScriptableObject
     {
-        public new void Destroy(Object target) => Destroy(target);
-        public new void DestroyImmediate(Object target) => DestroyImmediate(target);
+        public new void Destroy(Object target) => Object.Destroy(target);
+        public new void DestroyImmediate(Object target) => Object.DestroyImmediate(target);
     }
 }
